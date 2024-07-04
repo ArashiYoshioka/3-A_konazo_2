6278845d5050731f2937e4372d1fcb80979dbbe0
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-05 03:25:21 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkAnswer() {
+    const answerInput = document.getElementById('answerInput').value;
+    const result = document.getElementById('result');
+
+    if (answerInput === "スクエニ") {
+        result.textContent = "正解！";
+    } else {
+        result.textContent = "残念...";
+    }
+}
