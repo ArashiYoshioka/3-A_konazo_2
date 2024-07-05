afc4853285f37fc75490e7c0f9eb2b94a03359b9
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-05 14:43:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkAnswer() {
     const answerInput = document.getElementById('answerInput').value;
     const result = document.getElementById('result');
 
-    if (answerInput === "スクエニ") {
+    if (answerInput === "スクエア") {
         result.textContent = "正解！";
     } else {
         result.textContent = "残念...";
